3958a0b135cdf54c79962561ccb562d2809c7d8a
Lucas Schwendler <lucasschwendler@hotmail.com>
2024-03-27 23:31:28 +0000

    chore: Add docker completion for bash

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Configurando seu codespace..."
+
+#
+echo "Configurando docker completion for bash"
+echo "" >> ~/.bashrc
+echo "source <(docker completion bash)" >> ~/.bashrc
+
+source <(docker completion bash)
